0e441d33de6ce9c89d3f79c5be401b8c8f70392e
FerNaNdoNesi <fernandonesi@gmail.com>
2014-04-29 00:38:06 -0300

    ajuste database.php

--- Desenvolvimento/Projeto1/database/database.php
+++ Desenvolvimento/Projeto1/database/database.php
@@ -1,16 +1,20 @@
 <?php
 
-	/*
-	$host = "mysql9.000webhost.com";
+
+	$host = "mysql9.000webhost.com"; // local
 	$user = "a6233819_000web";
 	$pass = "000webhost";
-	*/
+	$app = "a6233819_000web"; // db
+
+	/*
 	$host = "localhost";
 	$user = "root";
 	$pass = "";
-	$app = "a6233819_000web";
+	$app = "projeto1";
+	*/
 	
 	mysql_connect($host, $user, $pass);
+	
 	mysql_select_db($app);
 
 	function dbConsulta($sql){
