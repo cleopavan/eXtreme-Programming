895d75fa1dde9c45f64671cc87356f496b8c8ba2
Jose <eduvenson@gmail.com>
2014-07-09 22:30:42 -0300

    cadastro e busca prontas, menos por curso

--- dsv/consultaCcr.php
+++ dsv/consultaCcr.php
@@ -18,7 +18,7 @@
 		
 		if ($_POST['filter'] == 'nome') {
 			$t = $_POST['text'];
-			$sql = "SELECT * FROM ccr WHERE nomeCcr='$t' AND regValido=1";
+			$sql = "SELECT * FROM ccr WHERE nomeCcr like '%$t%' AND regValido=1";
 			$retorno = dbConsulta($sql);
 			//$retornoArray = mysql_fetch_array($retorno);
 			//echo mysql_num_rows($retorno);
@@ -96,7 +96,42 @@
 			';	
 		}
 		if ($_POST['filter'] == 'ch'){
+			$t = $_POST['text'];
+			$sql = "SELECT * FROM ccr WHERE cHoraria='$t' AND regValido=1";
+			$retorno = dbConsulta($sql);
+			//$retornoArray = mysql_fetch_array($retorno);
+			//echo mysql_num_rows($retorno);
 			
+			echo '
+			<div class="panel panel-default">
+				  <!-- Default panel contents -->
+				  <div class="panel-heading">Resultado</div>
+
+				  <!-- Table -->
+				  <table class="table"> 
+					 <tr>
+					  <td>Nome</td>
+					  <td>Código</td> 
+					  <td>Carga Horária</td>
+					  <td>Domínio</td>
+					</tr> 
+					<tr>
+					</tr>';
+					if (mysql_num_rows($retorno) > 0) {
+						while($row = mysql_fetch_assoc($retorno)){
+							echo "<tr> 
+								<td>".$row['nomeCcr']."</td>	
+								<td>".$row['codCcr']."</td>	
+								<td>".$row['cHoraria']."</td>	
+								<td>".$row['idDominio']."</td>	
+							";
+							echo '</tr>';
+						}
+					}
+			echo '	  
+					</table>
+				</div>
+			';
 		}
 		if ($_POST['filter'] == 'domin'){
 			$t = $_POST['text'];
